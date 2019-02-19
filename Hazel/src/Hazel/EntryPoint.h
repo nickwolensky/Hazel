769930d025c8463e69cb7594a97c05d21d247e20
Nick Wolensky <nicholas.wolensky@gmail.com>
2019-02-19 06:30:41 -0500

    Added logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,11 @@ extern Hazel::Application* CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
 	auto app = CreateApplication();
 	app->Run();
 	delete app;
