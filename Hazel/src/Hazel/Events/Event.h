26dc8b81360a1c83dcab6cc6786d2a035764da16
nickwolensky <nicholas.wolensky@gmail.com>
2019-03-26 08:25:07 -0400

    Began working on the event system. Halfway through Cherno's event system video

---
+++ Hazel/src/Hazel/Events/Event.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Hazel/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Hazel {
+
+	// Events in Hazel are currently blocking, meaning when an event occurs it
+	// immediately gets dispatched and must be dealt with right then and there.
+	// For the future, a better strategy might be to buffer events in an event
+	// bus and process them during the "event" part of the update stage.
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WIndowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication	= BIT(0),
+		EventCategoryInput			= BIT(1),
+		EventCategoryKeyboard		= BIT(2),
+		EventCategoryMouse			= BIT(3),
+		EventCategoryMouseButton	= BIT(4)
+	};
+
+	class Hazel_API Event
+	{
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
+	protected:
+		bool m_handled = false;
+	};
+	
+}
---
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Event.h"
+
+#include <iostream>
+
+namespace Hazel {
+
+	class Hazel_API KeyEvent : public Event
+	{
+
+	};
+
+
+}
